898ce2a3df7dbd6dcbc7b0700af2a5323226b107
muhyidin3222 <muhyidin480@gmail.com>
2023-02-02 14:34:54 +0700

    update

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');    
 
 const r = require('./dist/index').makeCrud('cars', './storage');
+app.use(cors());
 app.use('/', r);
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3001, () => console.log('Example app listening on port 3001!'));
